4e10a83f9f51c7db1e400d6b2577897f8913243d
CaioAndersonMM <caioandersonmartins@gmail.com>
2023-12-28 16:05:54 -0300

    Função de Calcular Segurança da Senha

--- GeradorDeSenhas/script.js
+++ GeradorDeSenhas/script.js
@@ -3,6 +3,8 @@
         const passwordInput = document.querySelector("#password");
         const lengthText = document.querySelector("#lengthText");
         const inputCheckBox = document.querySelectorAll('input[type="checkbox"]');
+
+        const indicatorBar = document.querySelector('#security-bar');
        
         var checksboxes = {
             maiusculas: true,
@@ -12,7 +14,6 @@
         };
 
         function generatePassword(){
-       
     
             inputCheckBox.forEach(function (checkbox) {
                 if (checkbox.name in checksboxes) {
@@ -20,14 +21,12 @@
                 }
             });
 
-
             lengthText.innerText = "Tamanho: " + inputLength.value;
 
             var words = " abcdefghijklmnopqrstuvwxyzçABCDEFGYIJKLMNOPQRSTUVYXIZÇ1234567890!?@#$%&*-=+-., âéê";
 
             var password = "";
 
-            console.log(checksboxes.maiusculas)
             if (checksboxes.maiusculas == false) {
                 words = words.toLowerCase();
             }
@@ -49,7 +48,7 @@
                 password += words.substring(randomNumber, randomNumber + 1);
             }
             passwordInput.value = password;
-            console.log(password);
+            calculateSecurity();
         }
 
         generatePassword();
@@ -64,6 +63,41 @@
 
         document.getElementById("btnCopy1").addEventListener("click", copy);
         document.getElementById("btnCopy2").addEventListener("click", copy);
+        document.getElementById("reset").addEventListener("click", generatePassword);
+
+
+        function calculateSecurity(){
+            const percent = Math.round((inputLength.value/64) * 100);
+            if (inputLength.value < 8) {
+                indicatorBar.className = 'bar critical';
+            } else{
+                if(inputLength.value >= 32){
+                    indicatorBar.className = 'bar safe';
+                }
+                else if (checksboxes.maiusculas == true && checksboxes.numbers == true && checksboxes.simbols == true) {
+                    indicatorBar.className = 'bar safe';
+                } 
+                else if(inputLength.value >= 10 && checksboxes.simbols == true){
+                    indicatorBar.className = 'bar safe';
+                }
+
+                else if(inputLength.value >= 10 && (checksboxes.maiusculas == true && checksboxes.numbers == true)){
+                    indicatorBar.className = 'bar safe';
+                }
+
+                else if(checksboxes.maiusculas == true && checksboxes.numbers == false || checksboxes.simbols == false){
+                    indicatorBar.className = 'bar warning';
+                }
+                else if(checksboxes.simbols == true){
+                    indicatorBar.className = 'bar warning';
+                }
+                else if(inputCheckBox.length >=10 && checksboxes.simbols == true){
+                    indicatorBar.className = 'bar safe';
+                }
+            }
+            indicatorBar.style.width = `${percent}%`
+
+        }
 
         function copy() {
             navigator.clipboard.writeText(passwordInput.value)
